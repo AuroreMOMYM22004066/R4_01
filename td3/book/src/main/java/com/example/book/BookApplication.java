41f11600e7f53e257da3d2a014613396398f8710
m22004066 <au.momy2004@gmail.com>
2024-03-18 11:38:20 +0100

    init

---
+++ td3/book/src/main/java/com/example/book/BookApplication.java
@@ -0,0 +1,43 @@
+package com.example.book;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+@ApplicationScoped
+public class BookApplication extends Application {
+
+    /**
+     * Méthode appelée par l'API CDI pour injecter la connection à la base de données au moment de la création
+     * de la ressource
+     * @return un objet implémentant l'interface BookRepositoryInterface utilisée
+     *          pour accéder aux données des livres, voire les modifier
+     */
+    @Produces
+    private BookRepositoryInterface openDbConnection(){
+        BookRepositoryMariadb db = null;
+
+        try{
+            db = new BookRepositoryMariadb("jdbc:mariadb://mysql-phptd2.alwaysdata.net/phptd2_library_db", "phptd2_library", "Goldorak13790!");
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return db;
+    }
+
+    /**
+     * Méthode permettant de fermer la connexion à la base de données lorsque l'application est arrêtée
+     * @param bookRepo la connexion à la base de données instanciée dans la méthode @openDbConnection
+     */
+    private void closeDbConnection(@Disposes BookRepositoryInterface bookRepo ) {
+        bookRepo.close();
+    }
+}
